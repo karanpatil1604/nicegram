a3c4847faae609ffafcb8100d6df27c265ddbada
Karan Patil <karanpatil1604@gmail.com>
2023-12-14 12:50:07 +0530

    vercel hosting setup

--- nicegram_main/settings.py
+++ nicegram_main/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-pcss4&9-xfmbo!t6@-v&gcv5dmqon)-aq_ido@*kc0y9+u+5mw
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*', '.vercel.app']
+ALLOWED_HOSTS = ['.vercel.app', 'now.sh', '127.0.0.1', 'localhost']
 
 
 # Application definition
@@ -98,6 +98,20 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'nicegram',
+#         'USER': 'postgres',
+#         'PASSWORD': '',
+#         'HOST': '',
+#         'PORT': '',
+#     }
+# }
+
+# STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 
 
 # Password validation
