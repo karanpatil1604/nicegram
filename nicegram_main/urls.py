d42e9b618fb20845c4808875769af83cbef92121
Karan Patil <karanpatil1604@gmail.com>
2023-12-14 16:56:02 +0530

    homepage update

--- nicegram_main/urls.py
+++ nicegram_main/urls.py
@@ -14,12 +14,19 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from urllib import request
 from django.conf import settings
 from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
 
+
+def hello(request):
+    return "<h1>Hello World</h1>"
+
+
 urlpatterns = [
+    path('/', hello, name="homepage"),
     path('admin/', admin.site.urls),
     path('post/', include('posts.urls')),
     path('user/', include('accounts.urls')),
